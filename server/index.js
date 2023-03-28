05f1e77d421ac790fab67b872dc8f1820afd6e8d
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-03-28 09:37:04 +0700

    MongDB setup

--- server/index.js
+++ server/index.js
@@ -16,6 +16,14 @@ app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(express.json());
 
-console.log('hello')
 // Connect to MongoDB
-
+const PORT = process.env.PORT || 5000;
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(async () => {
+        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+    })
+    .catch((error) => console.log(error));
