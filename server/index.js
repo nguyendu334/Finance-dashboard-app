332a13c0a1b0c09cd84282df32da9250e2d72126
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-03-28 10:41:44 +0700

    Key Performance Indicators Route

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 
+import kpiRoutes from './routes/kpi.js';
+import KPI from './models/KPI.js';
+import { kpis } from './data/data.js';
+
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
@@ -16,6 +20,9 @@ app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(express.json());
 
+// routes
+app.use('/kpi', kpiRoutes);
+
 // Connect to MongoDB
 const PORT = process.env.PORT || 5000;
 mongoose
@@ -25,5 +32,9 @@ mongoose
     })
     .then(async () => {
         app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+        // add data ontim only or as needed
+        // await mongoose.connection.db.dropDatabase();
+        // KPI.insertMany(kpis);
     })
     .catch((error) => console.log(error));
