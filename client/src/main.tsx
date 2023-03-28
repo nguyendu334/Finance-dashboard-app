35e3a0c18946d2584afb467f3c4e003807979bfe
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-03-28 09:14:17 +0700

    Redux Toolkit Setup and Configuration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { api } from '@/state/api';
 import App from '@/App';
 import '@/index.css';
 
+export const store = configureStore({
+    reducer: { [api.reducerPath]: api.reducer },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
+});
+
+setupListeners(store.dispatch);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
+    <Provider store={store}>
         <App />
-    </React.StrictMode>,
+    </Provider>,
 );
